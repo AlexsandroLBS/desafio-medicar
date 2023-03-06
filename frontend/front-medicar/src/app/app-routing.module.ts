c817a7cee9b01188fe0974265c8910486f271bfd
AlexsandroLBS <alexsandro.lopes009@gmail.com>
2023-03-05 21:28:22 -0300

    update in routes

--- frontend/front-medicar/src/app/app-routing.module.ts
+++ frontend/front-medicar/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuConsultasComponent } from './pages/consultas/menu-consultas/menu-consultas.component';
+import { CadastroComponent } from './pages/user/cadastro/cadastro.component';
+import { LoginComponent } from './pages/user/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'menu', component: MenuConsultasComponent },
+  { path: '**', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
