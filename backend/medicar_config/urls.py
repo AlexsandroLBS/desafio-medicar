8da14c5a9c1a3687406d8586a5598a931cae5570
AlexsandroLBS <alexsandro.lopes009@gmail.com>
2023-03-05 21:27:21 -0300

    creating login and signup endpoints

--- backend/medicar_config/urls.py
+++ backend/medicar_config/urls.py
@@ -1,20 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
-from medicar.views import MedicosViewSet, ConsultasViewSet, UsuariosViewSet, AgendaViewSet, EspecialidadesViewSet, ConsultasDeleteViewSet
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-
-router.register(r'usuarios', UsuariosViewSet)
+from django.urls import path
+from medicar.views import MedicosViewSet, ConsultasViewSet, SignUpViewSet, AgendaViewSet, EspecialidadesViewSet, ConsultasDeleteViewSet, LoginViewSet
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('create_account/', SignUpViewSet.as_view()),
+    path('login/', LoginViewSet.as_view()),
     path('consultas/', ConsultasViewSet.as_view()),
     path('consultas/<int:id>/', ConsultasDeleteViewSet.as_view(), name='excluir_consulta'),
     path('agenda/', AgendaViewSet.as_view(), name='Agenda por médico'),
     path('medicos/', MedicosViewSet.as_view(), name='medicos por especialidade'),
     path('especialidades/', EspecialidadesViewSet.as_view(), name='Lista de especialidades'),
-    
 ]
