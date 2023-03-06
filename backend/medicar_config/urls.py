82ffabaeb00ed7ae90ee748355254eb0820bc77d
AlexsandroLBS <alexsandro.lopes009@gmail.com>
2023-03-06 17:11:24 -0300

    adjust in endpoints

--- backend/medicar_config/urls.py
+++ backend/medicar_config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from medicar.views import MedicosViewSet, ConsultasViewSet, SignUpViewSet, AgendaViewSet, EspecialidadesViewSet, ConsultasDeleteViewSet, LoginViewSet
+from medicar.views import MedicosViewSet, ConsultasViewSet, SignUpViewSet, AgendaViewSet, EspecialidadesViewSet, MedicoEspecialidade, ConsultasDeleteViewSet, LoginViewSet
 
 
 urlpatterns = [
@@ -9,7 +9,8 @@ urlpatterns = [
     path('login/', LoginViewSet.as_view()),
     path('consultas/', ConsultasViewSet.as_view()),
     path('consultas/<int:id>/', ConsultasDeleteViewSet.as_view(), name='excluir_consulta'),
-    path('agenda/', AgendaViewSet.as_view(), name='Agenda por médico'),
-    path('medicos/', MedicosViewSet.as_view(), name='medicos por especialidade'),
+    path('agendas/', AgendaViewSet.as_view(), name='Agenda por médico'),
+    path('medicos/', MedicosViewSet.as_view(), name='Médicos por especialidade'),
+    path('medico/', MedicoEspecialidade.as_view(), name='Especialidade por médico'),
     path('especialidades/', EspecialidadesViewSet.as_view(), name='Lista de especialidades'),
 ]
