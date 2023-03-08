caa8ebcf951c7971334a957cc88a291c2b89ce8e
AlexsandroLBS <alexsandro.lopes009@gmail.com>
2023-03-08 12:12:22 -0300

    adding validation for agendas

--- backend/medicar/admin.py
+++ backend/medicar/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from medicar.models import *
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 class Usuarios(admin.ModelAdmin):
     list_display = ('id', 'nome', 'senha')
@@ -11,9 +14,22 @@ class Medicos(admin.ModelAdmin):
     list_display_links = ('id', 'nome')
     search_fields = ('id', 'nome', 'crm', 'email')
 
+
+class AgendaForm(forms.ModelForm):
+    class Meta:
+        model = Agenda
+        fields = '__all__'
+
+    def clean_dia(self):
+        dia = self.cleaned_data['dia']
+        if dia <= timezone.localdate():
+            raise ValidationError("A data da agenda deve ser uma data futura.")
+        return dia
+    
+
 class Agendas(admin.ModelAdmin):
     list_display = ('id', 'medico', 'dia', 'horarios')
-    
+    form = AgendaForm
 
 admin.site.register(Medico, Medicos)
 admin.site.register(Usuario, Usuarios)
